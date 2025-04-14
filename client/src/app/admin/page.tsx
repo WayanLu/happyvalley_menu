3e28dc254f91aeb751d6555889f52ae2f148c0d1
WayanLu <ryliu3@gmail.com>
2025-04-13 20:06:28 -0700

    started admin page, got all items to show

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -1,10 +1,32 @@
+import menuData from "../data.json";
+
+interface MenuItem {
+  id: number;
+  name: string;
+  price: number | string;
+}
+
+interface MenuData {
+  menu: {
+    [category: string]: MenuItem[];
+  };
+}
+
 const AdminPage: React.FC = () => {
+  const typedMenuData = menuData as MenuData;
+
   return (
-    <div>
-      <h1>t</h1>
-      <h1>t</h1>
-      <h1>t</h1>
-      <h1>t</h1>
+    <div className="test">
+      {Object.entries(typedMenuData.menu).map(([category, items]) => (
+        <div key={category}>
+          <p>{category}</p>
+          <div>
+            {items.map((item) => (
+              <div key={item.id}>{item.name}</div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
